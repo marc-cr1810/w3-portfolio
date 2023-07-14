3e342cea6ed1ef35c1cebc0a681ab08348a0cb8b
marc-cr1810 <marc-cr1810@users.noreply.github.com>
2023-07-14 20:21:22 +1000

    Initial commit from W3Schools

---
+++ src/index.php
@@ -0,0 +1,22 @@
+<?php
+// If the request is made from our space preview functionality then turn on PHP error reporting
+if (isset($_SERVER['HTTP_X_FORWARDED_URL']) && strpos($_SERVER['HTTP_X_FORWARDED_URL'], '.w3spaces-preview.com/') !== false) {
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
+}
+?>
+
+<html>
+<head>
+<title>PHP blank template</title>
+<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<?php 
+  phpinfo();
+  echo '<h1>My awesome space</h1>'; 
+?>
+
+</body>
+</html>
